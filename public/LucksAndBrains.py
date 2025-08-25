b9cb80955c1264d697bda27a39c6c939f4f713d4
divyansh-69 <divyanshmsingh13@gmail.com>
2025-08-25 19:47:46 +0530

    initial commit

---
+++ public/LucksAndBrains.py
@@ -0,0 +1,92 @@
+import random
+print('Q.1 Who was Michael Faraday?')
+print('1.A Guitarist',end='  ')
+print('2.A Physicist',end='  ')
+print('3.A Pianist',end='  ')
+print('4.A Painter')
+y=int(input('Enter your answer(1/2/3/4):'))
+if y==2:
+    print('Hurrahh!!!,Absolutely spot onn!!!\
+ Now its time to test how lucky you are by playing lottery!!!!')
+    w=random.randint(1,6)
+    s=int(input('Choose any number from 1-5(Choose wisely!!!, You could win a lottery):'))
+    if w==s:
+        print('CONGRATULATIONS!!!,You did it, you won the lottery')
+    else:
+        print("Oops!!,looks like you couldn't guess the number.No worries,here's another question for you.")
+else:
+    print("Oops!!Looks like the answer was incorrect.But no worries, here's another question for you.")
+print('Q.2 Who is Taylor Swift?')
+print('1. A Ballerina',end='  ')
+print('2. An American singer',end='  ')
+print('3. A British singer',end='  ')
+print('4. A Swiss chef')
+z=int(input('Enter your answer(1/2/3/4):'))
+if z==2:
+    print('Hurrahh!!!,Absolutely spot onn!!!\
+ Now its time to play lottery!!!!')
+    p = random.randint(1, 6)
+    s = int(input('Choose any number from 1-5(Choose wisely!!!, You could win a lottery):'))
+    if p == s:
+        print('CONGRATULATIONS!!!,You did it, you won the lottery')
+
+    else:
+        print("Oops!!,looks like you couldn't guess the number.No worries,here's another question for you.")
+else:
+    print("Oops!!Looks like the answer was incorrect.But no worries, here's another question for you.")
+print('Q.3 Who is Vishwanathan Anand?')
+print('1.A Dancer',end='  ')
+print('2.A Chess Grandmaster',end='  ')
+print('3.A Footballer',end='  ')
+print('4.An Acrobat')
+b=int(input('Enter your answer(1/2/3/4):'))
+if b==2:
+    print('Hurrahh!!!,Absolutely spot onn!!!\
+ Now its time to play lottery!!!!')
+    c = random.randint(1, 6)
+    s = int(input('Choose any number from 1-5(Choose wisely!!!, You could win a lottery):'))
+    if c == s:
+        print('CONGRATULATIONS!!!,You did it, you won the lottery')
+
+    else:
+        print("Oops!!,looks like you couldn't guess the number.No worries,here's another question for you.")
+else:
+    print("Oops!!Looks like the answer was incorrect.But no worries, here's another question for you.")
+print('Q.4 Who is Kim Jong-un?')
+print('1.A Pilot',end='  ')
+print('2.A Violinist',end='  ')
+print('3.A Hollywood Director',end='  ')
+print('4.A North Korean Politician')
+a=int(input('Enter your answer(1/2/3/4):'))
+if a==4:
+    print('Hurrahh!!!,Absolutely spot onn!!!\
+ Now its time to play lottery!!!!')
+    q = random.randint(1, 6)
+    s = int(input('Choose any number from 1-5(Choose wisely!!!, You could win a lottery):'))
+    if q == s:
+        print('CONGRATULATIONS!!!,You did it, you won the lottery')
+
+    else:
+        print("Oops!!,looks like you couldn't guess the number.No worries,here's another question for you.")
+else:
+    print("Oops!!Looks like the answer was incorrect.But no worries, here's another question for you.")
+print('Q.5 "Kira" is the name of a charachter of which series?')
+print('1.Demon Slayer',end='  ')
+print('2.Kengan Ashura',end='  ')
+print('3.Death Note',end='  ')
+print('4.Dororo')
+x=int(input('Enter your answer(1/2/3/4):'))
+if x==3:
+    print('Hurrahh!!!,Absolutely spot onn!!!\
+ Now its time to play lottery!!!!')
+
+    s=int(input('Choose any number from 1-5(Choose wisely!!!, You could win a lottery)'))
+    if s==1:
+        print('CONGRATULATIONS!!!,You did it, you won the lottery')
+    else:
+        print("Hard Luck!!!, Looks like we're out of questions!!,Better luck next time.\
+ HOPE YOU ENJOYED OUR GAME")
+else:
+    print("OHH, Looks like were out of questions.Hard luck,you didn't even get the chance to play our lottery!!!\
+ Better luck next time!!!")
+    print('HOPE YOU ENJOYED OUR "LUCKS AND BRAINS"!!!')
